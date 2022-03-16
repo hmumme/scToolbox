666d8467da11bdc82ef5460b13b67eeebdd3daa5
Mumme, Hope Lauren <hopemumme@gmail.com>
2022-03-16 10:03:33 -0400

    load

--- R/cellAnno.R
+++ R/cellAnno.R
@@ -1,21 +1,33 @@
-## Automatic Cell Annotation Functions
+## Cell Annotation Functions
 
-#' Perform SingleR annotation on Seurat scRNA-seq Object
+#' Perform automatic SingleR annotation on Seurat scRNA-seq object
 #' 
-#' @param obj Seurat scRNA-seq object (default assay must be RNA, already normalized)
+#' @param obj Seurat scRNA-seq object (default assay must be RNA or SCT, already normalized and clustered)
 #' @param type A string specifying the type of annotation labels to produce (fine or broad labels)
 #' @return A vector of Labels to add into Seurat Object
 #' @export 
 #' @examples
 #' cellTypePredictions = scAnno(obj = seuratObj, type = "fine")
-#' seuratObj$cellAnnotation = cellTypePredictions
+#' seuratObj[["cellAnnotation"]] = cellTypePredictions
 scAnno <- function(obj, type) {
   hpca.se = celldex::HumanPrimaryCellAtlasData()
-  counts = GetAssayData(obj)
+  counts = Seurat::GetAssayData(obj)
   if (type == "fine") {
     pred = SingleR::SingleR(test = counts, ref = hpca.se, assay.type.test = 1, labels = hpca.se$label.fine)
   } else {
-    pred = SingleR::SingleR(test = counts, ref = hpca.se, assay.type.test = 1, labels = hpca.se$label)
+    pred = SingleR::SingleR(test = counts, ref = hpca.se, assay.type.test = 1, labels = hpca.se$label.main)
   }
   return(pred)
+}
+
+#'
+#'
+#' @param obj Seurat scRNA-seq object (default assay must be RNA or SCT, already normalized)
+#' @param clusters clusters of interest to provide dotplot (default is all)
+#' @param type A string specifiying the type of annotation
+#' @export
+#' 
+#' 
+dotAnno = function(obj, clusters = "all", type = "all") {
+  
 }
---
+++ load.R
@@ -0,0 +1,28 @@
+#' Load example Seurat datasets for testing
+#' 
+#' @param ex Seurat example dataset to load, options include: "3k"
+#' @param norm A boolean specifying whether to normalize RNA assay and cluster (default FALSE)
+#' @return Seurat object of example dataset
+#' @examples  
+#' obj = loadExample("3k")
+#' 
+loadExample = function(ex, norm = FALSE) {
+  if (ex == "3k") {
+    data = Seurat::Read10X(data.dir = "data/pbmc3k/filtered_gene_bc_matrices/hg19/")
+  } else {
+    print("dataset does not exist, available: 3k")
+    return(NULL)
+  } 
+    obj = Seurat::CreateSeuratObject(counts = data, project = ex, min.cells = 3, min.features = 200)
+    if (norm) {
+      obj <- Seurat::NormalizeData(obj)
+      obj <- Seurat::FindVariableFeatures(obj)
+      obj <- Seurat::ScaleData(obj)
+      obj <- Seurat::RunPCA(obj, features = Seurat::VariableFeatures(object = obj))
+      obj <- Seurat::FindNeighbors(obj, dims = 1:10)
+      obj <- Seurat::FindClusters(obj, resolution = 0.5)
+      obj <- Seurat::RunUMAP(obj, dims = 1:10)
+    } else {
+      return(obj)
+    }
+}
